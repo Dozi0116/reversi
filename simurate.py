f984bd2cf544e0350c03e7934a2fff7777d17747
Dozi0116 <oi.dozi0116@gmail.com>
2019-05-31 13:21:30 +0900

    [fix] 棋譜を最後にまとめてコンソールに表示する

--- simurate.py
+++ simurate.py
@@ -14,11 +14,13 @@ def main():
     game.game_init()
 
     # game main loop
+    score = ''
     while True:
         # print('PLAYER {} TURN'.format(game.ORDER.index(game.turn)+1))
         pos = game.turn.rogic(game)
         # print('put position -> ({}, {})'.format(pos[0], pos[1]))
-        print('{}{}'.format(chr(ord('A') + (pos[0] - 1)), pos[1]), end='')
+        # print('{}{}'.format(chr(ord('A') + (pos[0] - 1)), pos[1]), end='')
+        score += chr(ord('A') + (pos[0] - 1)) +  str(pos[1])
 
         game.put_stone(pos, game.turn)
         
@@ -41,6 +43,7 @@ def main():
         if len(game.putlist) == 0:
             # 終了
             game.end()
+            print(score)
             return
 
     return
