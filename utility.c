7b1fb02bc0c4dcaccc84be2bd9cf48bc7a2c03cb
Dozi0116 <oi.dozi0116@gmail.com>
2019-10-08 12:07:05 +0900

    [add] utility functions

--- utility.c
+++ utility.c
@@ -51,3 +51,35 @@ int make_putlist(char reverse[BOARD_SIZE+2][BOARD_SIZE+2], int putlist[][2]) {
 
     return length;
 }
+
+int len(int arr[]) {
+    return sizeof(arr) / sizeof(arr[0]);
+}
+
+int len(double arr[]) {
+    return sizefo(arr) / sizeof(arr[0]);
+}
+
+int min(int arr[]) {
+    int i = 0;
+    int num = arr[0];
+    for (i = 0;i < len(arr);i++) {
+        if (num > arr[i]) {
+            num = arr[i];
+        }
+    }
+
+    return num;
+}
+
+int min(double arr[]) {
+    int i = 0;
+    int num = arr[0];
+    for (i = 0;i < len(arr);i++) {
+        if (num > arr[i]) {
+            num = arr[i];
+        }
+    }
+
+    return num;
+}
--- utility.h
+++ utility.h
@@ -6,5 +6,9 @@
 extern int c2i(char c);
 extern int input(char command[]);
 extern int make_putlist(char reverse[BOARD_SIZE+2][BOARD_SIZE+2], int putlist[][2]);
+extern int len(int arr[]);
+extern int len(double arr[]);
+extern int min(int arr[]);
+extern int min(double arr[]);
 
 #endif
