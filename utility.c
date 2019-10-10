307b3678313b2fed2c6dcf6a66dfc1c46b2a6e84
Dozi0116 <oi.dozi0116@gmail.com>
2019-10-10 14:53:38 +0900

    [clean] 関数を適切な位置に移動させた

--- utility.c
+++ utility.c
@@ -36,75 +36,20 @@ void input(char command[]) {
     }
 }
 
-int make_putlist(char reverse[BOARD_SIZE+2][BOARD_SIZE+2], int putlist[][2]) {
-    int length = 0;
-    int x, y;
-    for (y = 1;y < BOARD_SIZE+1;y++) {
-        for (x = 1;x < BOARD_SIZE+1;x++) {
-            if (reverse[y][x] != 0) {
-                putlist[length][0] = y;
-                putlist[length][1] = x;
-                length++;
-            }
-        }
-    }
 
-    return length;
-}
-
-int make_reverse_test(char board[BOARD_SIZE+2][BOARD_SIZE+2],
-                 int player,
-                 char reverse[BOARD_SIZE+2][BOARD_SIZE+2],
-                 int putlist[][2]) {
-    /*
-    playerとboardからひっくり返せる場所を見つけて返す。
-    返り値は置けるかどうか。TRUEで置ける。
-    */
-
-   int y, x;
-   int dy, dx;
-   int i;
-   int index = 0;
-
-    for (y = 1;y <= BOARD_SIZE;y++) {
-        for (x = 1;x <= BOARD_SIZE;x++) {
-            reverse[y][x] = 0; // 初期化
-            if (board[y][x] != EMPTY) continue;
-            for (i = 0;i < 8;i++) {
-                dy = y + DIRECTION[i][0];
-                dx = x + DIRECTION[i][1];
-                if (board[dy][dx] == opponent(player)) {
-                    do {
-                        dy += DIRECTION[i][0];
-                        dx += DIRECTION[i][1];
-                    } while (board[dy][dx] == opponent(player));
-                    
-                    if (board[dy][dx] == player) {
-                        reverse[y][x] |= FLAG[i];
-                        putlist[index][0] = y;
-                        putlist[index][1] = x;
-                        index++;
-                    }
-                }
-            }
-        }
-    }
-
-    return index;
-}
 
 int len_i(int arr[]) {
     return sizeof(arr) / sizeof(arr[0]);
 }
 
 int len_d(double arr[]) {
-    return sizefo(arr) / sizeof(arr[0]);
+    return sizeof(arr) / sizeof(arr[0]);
 }
 
 int min_i(int arr[]) {
     int i = 0;
     int num = arr[0];
-    for (i = 0;i < len(arr);i++) {
+    for (i = 0;i < len_i(arr);i++) {
         if (num > arr[i]) {
             num = arr[i];
         }
@@ -116,7 +61,7 @@ int min_i(int arr[]) {
 int min_d(double arr[]) {
     int i = 0;
     int num = arr[0];
-    for (i = 0;i < len(arr);i++) {
+    for (i = 0;i < len_d(arr);i++) {
         if (num > arr[i]) {
             num = arr[i];
         }
--- utility.h
+++ utility.h
@@ -5,8 +5,6 @@
 
 extern int c2i(char c);
 extern int input(char command[]);
-extern int make_putlist(char reverse[BOARD_SIZE+2][BOARD_SIZE+2], int putlist[][2]);
-extern int make_reverse_test(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player, char reverse[BOARD_SIZE+2][BOARD_SIZE+2], int putlist[][2]);
 extern int len_i(int arr[]);
 extern int len_d(double arr[]);
 extern int min_i(int arr[]);
