a29d603b0262e5d0f6f523042f75f6fb5dcea7ab
Dozi0116 <oi.dozi0116@gmail.com>
2019-11-21 10:26:17 +0900

    [add]  eval_put_position

--- eval.c
+++ eval.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include "eval.h"
 
+#define SB_WEIGHT 1
+#define PP_WEIGHT 1
+#define OS_WEIGHT 1
+
 /*
     各種評価用の関数一覧。関数名はeval_xxxとする(evalのみ例外)。
     引数
@@ -20,8 +24,8 @@
 double eval_static_board(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player) {
     // 静的盤面評価
     // 予め、マスごとに決められた点数を加算する。
-    // (先輩のプログラムは自分の石だけ見ていたから、今回は自分の石だけ考慮)
-    const int board_score[BOARD_SIZE+2][BOARD_SIZE+2] = {
+
+    static const int board_score[BOARD_SIZE+2][BOARD_SIZE+2] = {
         {0,   0,   0,   0,   0,   0,   0,   0,   0, 0},
         {0, 100, -50,  10,   0,   0,  10, -50, 100, 0},
         {0, -50, -70,  -5, -10, -10,  -5, -70, -50, 0},
@@ -51,9 +55,49 @@ double eval_static_board(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player) {
     return score;
 }
 
+double eval_put_position(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player) {
+    // 着手可能手数
+    // 自分が置ける手数 - 相手が置ける手数
+    // https://github.com/nehori/c-othello/blob/master/source/eval.c
+    // 上記のURLの評価関数を参考にした。
+    // http://sealsoft.jp/thell/algorithm.html
+    // ここを参照すると5〜16手目まではとても寄与するが、それ以外はほとんど寄与しない
+    // 着手可能数の重みは手数によって変更する必要がありそう
 
-double eval(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player) {
-    double esb_score = eval_static_board(board, player);
-    //printf("return score -> %f\n", esb_score);
-    return esb_score;
+    char reverse[BOARD_SIZE+2][BOARD_SIZE+2];
+    char putlist[30][2];
+    int i, length;
+
+    int my_score = make_board_to_putlist(board, player, reverse, putlist);
+    for (i = 0, length = my_score;i < length;i++) {
+        if ((putlist[i][0] == 2 && putlist[i][1] == 2)
+         || (putlist[i][0] == 2 && putlist[i][1] == 7)
+         || (putlist[i][0] == 7 && putlist[i][1] == 2)
+         || (putlist[i][0] == 7 && putlist[i][1] == 7)) {
+                my_score--; // x打ち(角の内側のところ)は着手可能であってもアウト
+        }
+    }
+
+    int opponent_score = make_board_to_putlist(board, opponent(player), reverse, putlist);
+    for (i = 0, length = opponent_score;i < length;i++) {
+        if ((putlist[i][0] == 2 && putlist[i][1] == 2)
+         || (putlist[i][0] == 2 && putlist[i][1] == 7)
+         || (putlist[i][0] == 7 && putlist[i][1] == 2)
+         || (putlist[i][0] == 7 && putlist[i][1] == 7)) {
+                opponent_score--; // x打ち(角の内側のところ)は着手可能であってもアウト
+        }
+    }
+
+    return my_score - opponent_score;
+}
+
+
+double eval(char board[BOARD_SIZE+2][BOARD_SIZE+2], int player,
+            int pos[2], char putted_board[BOARD_SIZE+2][BOARD_SIZE+2]) {
+    // 静的盤面評価
+    double sb_score = eval_static_board(putted_board, player) * SB_WEIGHT;
+    // 着手可能手数
+    double pp_score = eval_put_position(board, player) * PP_WEIGHT;
+
+    return sb_score + pp_score;
 }
