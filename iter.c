6359ef03a3f7cde8c885acb588a419a075736cd2
Dozi0116 <oi.dozi0116@gmail.com>
2020-01-14 08:16:22 +0900

    [add] 実験結果のファイルを追加した

--- iter.c
+++ iter.c
@@ -38,8 +38,7 @@ int main(int argc, char *argv[]) {
 
     Game game;
 
-
-    const int match_max = 500;
+    const int match_max = 100;
 
     int total_win = 0, total_lose = 0;
     int i, j, k, stone, win = 0, lose = 0, result = 0;
@@ -49,7 +48,7 @@ int main(int argc, char *argv[]) {
     for (match = 0;match < match_max;match++){
         game_init(&game);
 
-        // printf("game %d\n", match+1);
+        fprintf(stderr, "game %d\n", match+1);
 
         while (TRUE) {
             // printf("\tturn %d\n", game.stone_num-3);
--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 rand=2019
+# rand = 2019 + 1 + 2 + ... + 45 = 3054
+rand=3054
 
 for i in `seq 1 100`
 do
-    rand=`expr $rand + $i`
     for j in 1 2
     do
         echo "playing ./test $j $i $rand"
