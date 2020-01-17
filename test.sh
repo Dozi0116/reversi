34455ea7a77286b568f503a3f03d2457a2958441
Dozi0116 <oi.dozi0116@gmail.com>
2020-01-17 10:13:47 +0900

    [fix] test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@
 
 rand=2019
 # rand = 2019 + 1 + 2 + ... + 45 = 3054
-rand=3054
+# rand = 3054
 
 for i in `seq 1 100`
 do
+    rand=`expr $rand + $i`
     echo "playing ./test $i $rand"
     echo "T = $i rand = $rand"
     ./test $i $rand >> result.dat
