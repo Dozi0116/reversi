c3f35850301fdd71b6c6b32bc0cff8e7ca0eb75f
dozi0116 <oi.dozi0116@gmail.com>
2020-02-25 12:07:01 +0900

    [update] .gitignore + 消えてたファイルを戻した

---
+++ test/iter2-2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##########
+# i=8 # max winrate
+i=84 # min winrate
+##########
+rand=8192
+echo "playing ./iter_test $i $rand"
+echo "T = $i rand = $rand"
+./iter_test2 $i $rand >> iter2-2.csv
+
---
+++ test/iter5.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# rand=2019
+# rand = 2019 + 1 + 2 + ... + 45 = 3054
+# rand = 3054
+
+#####
+# rand=3054 # iter 1
+# rand=2020 # iter 2
+# rand=116 # iter 3
+# rand=182 # iter 4
+rand=1 # iter 5
+#####
+
+for i in `seq 1 100`
+do
+    # rand=`expr $rand + $i`
+    echo "playing ./iter_test $i $rand"
+    echo "T = $i rand = $rand"
+    ./iter_test $i $rand >> iter5.csv
+done
+
